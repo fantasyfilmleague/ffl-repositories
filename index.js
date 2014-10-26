84f286460595d124b6567ef58c15dc7f6a63c6ce
Colin Teal <the30yearswar@hotmail.com>
2014-10-25 19:33:45 -0700

    Add a team repository, specify created at for seasons

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.repository = require('./lib/repository');
 exports.user = require('./lib/user');
 exports.league = require('./lib/league');
 exports.season = require('./lib/season');
+exports.team = require('./lib/team');
