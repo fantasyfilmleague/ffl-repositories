d8f3cb454c7431801cdc24c4af0b395d6f0c8f61
Colin Teal <the30yearswar@hotmail.com>
2014-10-26 22:00:21 -0700

    League creation takes a founder, tests

---
+++ test/integration/lib/league.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var assert = require('assert');
+var Chance = require('chance');
+var user = require('../../../lib/user');
+var league = require('../../../lib/league');
+
+describe('LeagueRepository', function () {
+  var repository, chance, userRepository;
+
+  beforeEach(function () {
+    chance = new Chance();
+    repository = league.createRepository();
+    userRepository = user.createRepository();
+  });
+
+  describe('#create', function () {
+
+    it('should create a league', function (done) {
+      userRepository.create(chance.email(), chance.word(), function (error, user) {
+        assert.ifError(error);
+
+        repository.create(user.id, chance.sentence(), function (error, league) {
+          assert.ifError(error);
+          assert.ok(user);
+          done();
+        });
+      });
+    });
+
+  });
+
+});
