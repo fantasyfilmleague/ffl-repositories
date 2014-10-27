413c294b4c9e7b6d69cafb00482924ccbab15a92
Colin Teal <the30yearswar@hotmail.com>
2014-10-26 21:45:33 -0700

    Add find by email tests

--- test/integration/lib/user.js
+++ test/integration/lib/user.js
@@ -1,21 +1,49 @@
 'use strict';
 
 var assert = require('assert');
+var Chance = require('chance');
 var user = require('../../../lib/user');
 
 describe('UserRepository', function () {
-  var repository;
+  var repository, chance;
 
   beforeEach(function () {
+    chance = new Chance();
     repository = user.createRepository();
   });
 
   describe('#create', function () {
 
     it('should create a user', function (done) {
-      repository.create('test@user.com', 'pass', function (error, user) {
+      repository.create(chance.email(), chance.word(), function (error, user) {
         assert.ifError(error);
         assert.ok(user);
+        done();
+      });
+    });
+
+  });
+
+  describe('#findByEmail', function () {
+
+    it('should find user by email if it exists', function (done) {
+      var email = chance.email();
+
+      repository.create(email, chance.word(), function (error) {
+        assert.ifError(error);
+
+        repository.findByEmail(email, function (error, user) {
+          assert.ifError(error);
+          assert.ok(user);
+          done();
+        });
+      });
+    });
+
+    it('should not find user by email if it does not exist', function (done) {
+      repository.findByEmail(chance.email(), function (error, user) {
+        assert.ifError(error);
+        assert.ok(!user);
         done();
       });
     });
