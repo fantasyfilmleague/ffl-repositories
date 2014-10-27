180f34e40921d7aab62f5de84350f467f25efaf5
Colin Teal <the30yearswar@hotmail.com>
2014-10-26 21:17:12 -0700

    Add members repository, Travis CI

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 exports.repository = require('./lib/repository');
 exports.user = require('./lib/user');
 exports.league = require('./lib/league');
+exports.member = require('./lib/member');
 exports.season = require('./lib/season');
 exports.team = require('./lib/team');
