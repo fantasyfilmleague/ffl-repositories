391ca6d3e3923510cf0d55dc7884d4d24037745e
Colin Teal <the30yearswar@hotmail.com>
2014-10-26 21:29:57 -0700

    Add an integration test for user repository

---
+++ test/integration/lib/user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var assert = require('assert');
+var user = require('../../../lib/user');
+
+describe('UserRepository', function () {
+  var repository;
+
+  beforeEach(function () {
+    repository = user.createRepository();
+  });
+
+  describe('#create', function () {
+
+    it('should create a user', function (done) {
+      repository.create('test@user.com', 'pass', function (error, user) {
+        assert.ifError(error);
+        assert.ok(user);
+      });
+    });
+
+  });
+
+});
