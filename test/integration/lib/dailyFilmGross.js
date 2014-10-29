743f898d4b289b979b8ebf12eb809c3a487930e8
Colin Teal <the30yearswar@hotmail.com>
2014-10-29 00:22:09 -0700

    Add a daily film gross repository

---
+++ test/integration/lib/dailyFilmGross.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var assert = require('assert');
+var Chance = require('chance');
+var moment = require('moment');
+var film = require('../../../lib/film');
+var dailyFilmGross = require('../../../lib/dailyFilmGross');
+
+describe('DailyFilmGrossRepository', function () {
+  var filmRepository, repository, chance;
+
+  beforeEach(function () {
+    chance = new Chance();
+    filmRepository = film.createRepository();
+    repository = dailyFilmGross.createRepository();
+  });
+
+  function createFilm(callback) {
+    filmRepository.create(chance.sentence(), moment.utc(), function (error, film) {
+      assert.ifError(error);
+      callback(film);
+    });
+  }
+
+  describe('#create', function () {
+
+    it('should create a daily film gross', function (done) {
+      createFilm(function (film) {
+        repository.create(film.id, chance.natural(), moment.utc(), function (error, gross) {
+          assert.ifError(error);
+          assert.ok(gross);
+          done();
+        });
+      });
+    });
+
+  });
+
+});
