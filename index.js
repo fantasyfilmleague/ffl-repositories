2f9f865687ebf6257ac8b12cc4c1b83931b14638
Colin Teal <the30yearswar@hotmail.com>
2014-10-28 23:46:54 -0700

    Use require by directory utility

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-exports.repository = require('./lib/repository');
-exports.user = require('./lib/user');
-exports.league = require('./lib/league');
-exports.member = require('./lib/member');
-exports.season = require('./lib/season');
-exports.team = require('./lib/team');
+var path = require('path');
+var libPath = path.join(__dirname, './lib');
+
+require('ffl-utils').requireByDir(__filename, libPath, exports);
