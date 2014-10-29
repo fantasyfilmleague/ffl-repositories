a00850c154781a2cb2ee3c08d5c939d53a989bed
Colin Teal <the30yearswar@hotmail.com>
2014-10-29 00:05:40 -0700

    Add a cast repository

---
+++ test/integration/lib/cast.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var assert = require('assert');
+var Chance = require('chance');
+var moment = require('moment');
+var user = require('../../../lib/user');
+var actor = require('../../../lib/actor');
+var film = require('../../../lib/film');
+
+describe('CastRepository', function () {
+  var actorRepository, filmRepository, repository, chance;
+
+  beforeEach(function () {
+    chance = new Chance();
+    actorRepository = actor.createRepository();
+    filmRepository = film.createRepository();
+    repository = user.createRepository();
+  });
+
+  function createFilmActor(callback) {
+    actorRepository.create(chance.name(), function (error, actor) {
+      assert.ifError(error);
+      filmRepository.create(chance.sentence(), moment.utc(), function (error, film) {
+        assert.ifError(error);
+        callback(film, actor);
+      });
+    });
+  }
+
+  describe('#create', function () {
+
+    it('should create a user', function (done) {
+      createFilmActor(function (film, actor) {
+        repository.create(film.id, actor.id, function (error, cast) {
+          assert.ifError(error);
+          assert.ok(cast);
+          done();
+        });
+      });
+    });
+
+  });
+
+});
